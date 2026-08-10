3212c0c59ed7b1925070ba133db8fe17cd2e32f1
Helmy Alfarizy <emailanda@example.com>
2026-08-10 09:02:02 +0700

    test: automatic Laravel deployment v2

--- laravel/routes/web.php
+++ laravel/routes/web.php
@@ -1,11 +1,3 @@
-<?php
-
-use Illuminate\Support\Facades\Route;
-
-Route::get('/', function () {
-    return '
-        <h1>CI/CD Laravel 7 Berhasil 🚀</h1>
-        <p>Version: 1.0</p>
-        <p>Environment: Production</p>
-    ';
-});
+<h1>CI/CD Laravel 7 Berhasil 🚀</h1>
+<p>Version: 2.0</p>
+<p>Auto Deploy Test Berhasil</p>
